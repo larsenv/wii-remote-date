72494231a325f00d92309d5d3e2096ea687c6e19
Sourcery AI <>
2023-10-27 16:19:20 +0000

    'Refactored by Sourcery'

--- wii-remote-date.py
+++ wii-remote-date.py
@@ -3,16 +3,12 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: python wii-remote-date.py <date string>")
     sys.exit(1)
- 
+
 if len(sys.argv[1]) < 3:
     print("Invalid string")
     sys.exit(1)
 
-if sys.argv[1][0] == "Z":
-    year = "2006"
-else:
-    year = ord(sys.argv[1][0]) + 1942
-
+year = "2006" if sys.argv[1][0] == "Z" else ord(sys.argv[1][0]) + 1942
 if ord(sys.argv[1][1]) >= 49 and ord(sys.argv[1][1]) <= 57:
     month = sys.argv[1][1]
 elif sys.argv[1][1] == "O":
@@ -28,12 +24,12 @@ else:
     day = ord(sys.argv[1][2]) - 55
 
 if int(year) > 2016:
-    print("Your year is probably incorrect: " + str(year))
+    print(f"Your year is probably incorrect: {str(year)}")
 
 if int(month) > 12:
-    print("Your month is probably incorrect: " + str(month))
+    print(f"Your month is probably incorrect: {str(month)}")
 
 if int(day) > 31:
-    print("Your day is probably incorrect: " + str(day))
+    print(f"Your day is probably incorrect: {str(day)}")
 
 print(f"{month}/{day}/{year}")
